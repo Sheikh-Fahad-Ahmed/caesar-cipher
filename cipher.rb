cb2f4d88d54026cbef21e237167c4394f9faa630
Fahad Ahmed <fahad838@gmail.com>
2024-08-26 11:15:33 +0530

    Complete coding of caesar cipher

--- cipher.rb
+++ cipher.rb
@@ -1,12 +1,21 @@
-require 'pry'
 
 def caesar_cipher(string, key)
-  ascii_code = string.bytes
-  ascii_code.map do |num|
-    num += key
+  ascii_code = []
+  string.each_byte do |c|
+    if c >= 65 && c <=90
+      c += key
+      if c > 90
+        c -= 26
+      end
+    elsif c >= 97 && c <= 122
+      c += key
+      if c > 122
+        c-= 26
+      end
+    end
+    ascii_code.push(c)
   end
-  ascii_code.pack(c*)
-  binding.pry
+  ascii_code.pack('c*')
 end
 
-puts caesar_cipher("string")
+puts caesar_cipher("What a string!", 5)

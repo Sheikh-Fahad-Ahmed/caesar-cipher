d0f9b9dee9bdd64ed32de4e70c767c2ba29e5d07
Fahad Ahmed <fahad838@gmail.com>
2024-09-01 13:31:26 +0530

    Test rubocop style guide

--- cipher.rb
+++ cipher.rb
@@ -1,21 +1,18 @@
+# frozen_string_literal: true
 
-def caesar_cipher(string, key)
+def cipher(string, key)
   ascii_code = []
   string.each_byte do |c|
-    if c >= 65 && c <=90
+    if c >= 65 && c <= 90
       c += key
-      if c > 90
-        c -= 26
-      end
+      c -= 26 if c > 90
     elsif c >= 97 && c <= 122
       c += key
-      if c > 122
-        c-= 26
-      end
+      c -= 26 if c > 122
     end
     ascii_code.push(c)
   end
   ascii_code.pack('c*')
 end
 
-puts caesar_cipher("What a string!", 5)
+puts cipher('What a string!', 5)

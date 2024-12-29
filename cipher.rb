2e9870c41dab05d453fc0c7ddb8f280de81eec67
Fahad Ahmed <fahad838@gmail.com>
2024-12-29 18:43:18 +0530

    Refactor cipher method into a class
    Refactored cipher method into cipher class for
    better understandability.

--- cipher.rb
+++ cipher.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 
-
-def cipher(string, key)
-  ascii_code = []
-  string.each_byte do |c|
-    if c >= 65 && c <= 90
-      c += key
-      c -= 26 if c > 90
-    elsif c >= 97 && c <= 122
-      c += key
-      c -= 26 if c > 122
+class Cipher
+  def cipher(string, key)
+    ascii_code = []
+    string.each_byte do |c|
+      if c >= 65 && c <= 90
+        c += key
+        c -= 26 if c > 90
+      elsif c >= 97 && c <= 122
+        c += key
+        c -= 26 if c > 122
+      end
+      ascii_code.push(c)
     end
-    ascii_code.push(c)
+    ascii_code.pack("c*")
   end
-  ascii_code.pack("c*")
 end
-
 puts cipher("What a string!", 5)

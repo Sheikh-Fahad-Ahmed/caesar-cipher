c0406afebfa32acde2bdab7219f4bcfbe6da3ceb
Fahad Ahmed <fahad838@gmail.com>
2024-12-30 10:28:39 +0530

    Finish coding a test that passes the program.

--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -1,2 +1,12 @@
-require_relative 'cipher'
+require 'spec_helper.rb'
+require './cipher.rb'
 
+describe Cipher do
+  context "runs the program" do
+    subject(:transform) { described_class.new }
+    it "returns an ascii code for a sentence" do
+      result = transform.cipher('What a String!', 5).force_encoding(Encoding::UTF_8)
+      expect(result).to eq("Bmfy f Xywnsl!")
+    end
+  end
+end

5adb71df0457053aeb75d4e45092fb2f76c2d804
Fahad Ahmed <fahad838@gmail.com>
2024-12-30 10:32:10 +0530

    Finish testing the project

--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -5,8 +5,13 @@ describe Cipher do
   context "runs the program" do
     subject(:transform) { described_class.new }
     it "returns an ascii code for a sentence" do
-      result = transform.cipher('What a String!', 5).force_encoding(Encoding::UTF_8)
+      result = transform.cipher('What a String!', 5)
       expect(result).to eq("Bmfy f Xywnsl!")
+    end
+
+    it "Hello returns Mjqqt" do
+      result = transform.cipher("Hello", 5)
+      expect(result).to eq("Mjqqt")
     end
   end
 end

949a71df99bd161e8a0bbdc56105466e3d6aad36
Fahad Ahmed <fahad838@gmail.com>
2024-12-30 10:32:39 +0530

    Remove hardcoded initialzation

--- cipher.rb
+++ cipher.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-
 class Cipher
   def cipher(string, key)
     ascii_code = []
@@ -16,12 +15,6 @@ class Cipher
       end
       ascii_code.push(value.chr)
     end
-    ascii_code.join('')
+    ascii_code.join
   end
 end
-
-
-
-c = Cipher.new
-puts c.cipher('What a String!', 5)
-puts c.cipher('What a String!', 5) == "Bmfy f Xywnsl!" ? "true" : "false"
